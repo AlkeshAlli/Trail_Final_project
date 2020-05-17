29bc876a015b359c31c32aa56d5af5e817eec95c
rohith996 <dachavaram.rohithkumar@gmail.com>
2020-05-17 07:55:39 -0400

    initial commit

--- app/src/main/java/com/example/finalprojectgroup8/MainActivity.java
+++ app/src/main/java/com/example/finalprojectgroup8/MainActivity.java
@@ -7,13 +7,16 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView logo;
     private static int time_out = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logo = findViewById(R.id.logo);
 
         new Handler().postDelayed(new Runnable() {
             @Override
